091c796465026ede61da21cf546c04aca30b01e0
Unknown <hatiyliess86@gmail.com>
2017-12-06 12:33:43 +0100

    Remove Each Test

--- debug_drawings.py
+++ debug_drawings.py
@@ -16,35 +16,3 @@ class DebugDrawings(object):
 	@staticmethod
 	def draw_line(screen, start_point, end_point, color):
 		draw.line(screen, color, (start_point.x, start_point.y), (end_point.x, end_point.y), 2)
-
-# ===================================================
-# EXAMPLE
-
-if __name__ == '__main__':
-	from pygame import display
-	from time import clock
-	from vector import Vector 
-
-	width = 800
-	height = 600
-	BLACK = (0, 0, 0)
-	screen = display.set_mode((width, height))
-	screen.fill(BLACK)
-
-	current_time = clock()
-	dt = 0
-
-	while True:
-		screen.fill(BLACK)
-		dt = clock() - current_time
-		current_time += dt
-
-		DebugDrawings.draw_circle(screen, Vector(400, 300), 50, (255, 0, 0))
-		DebugDrawings.draw_circle(screen, Vector(350, 250), 20, (0, 0, 255))
-		DebugDrawings.draw_circle(screen, Vector(450, 250), 20, (0, 0, 255))
-		DebugDrawings.draw_circle(screen, Vector(400, 370), 10, (0, 0, 255))
-		DebugDrawings.draw_rect(screen, Vector(400, 300), 200, 200, (0, 255, 0))
-
-		DebugDrawings.draw_line(screen, Vector(0, 0), Vector(800, 600), (255, 255, 0))
-
-		display.flip()
