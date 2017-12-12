aeb50f99bbae7905cee26977b2c28cda865f99b5
Medhi Sam <sammedhi2@hotmail.fr>
2017-12-12 02:11:35 +0100

    Object finding game implement , it'll later on become a pacman

--- spawner.py
+++ spawner.py
@@ -1,11 +1,13 @@
 from abc import ABCMeta, abstractmethod
 
 class Spawner:
-	__metaclass__ = ABCMeta
-	def __init__(self,n,map):
-		self.spawn(n,map)
+    __metaclass__ = ABCMeta
+    def __init__(self):
+        pass
 
-	@abstractmethod
-	def spawn(self,n,map):
-		pass
+    @abstractmethod
+    def spawn(self,spawn_number,map):
+        pass
 
+    
+                
