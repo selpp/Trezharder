e71b7db47bd00527d5825b8ac5e192174ac95445
Medhi Sam <sammedhi2@hotmail.fr>
2017-12-29 21:08:20 +0100

    The goal have a trigger collider

--- goal.py
+++ goal.py
@@ -7,6 +7,6 @@ class Goal(MonoBehaviour):
         MonoBehaviour.__init__(self,1)
     
     def start(self):
-        BoxCollider(self.transform,Vector(0,0),Vector(1,1),self.gameobject)
+        BoxCollider(self.transform,Vector(0,0),Vector(1,1),self.gameobject,True)
     
     
